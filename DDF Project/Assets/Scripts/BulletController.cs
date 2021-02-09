0ae94ee85edc3262c3bea3067ec577696cd80f77
Chenshu Xu <newton9001@gmail.com>
2021-02-09 14:33:42 -0800

    Updated bullet movement

--- DDF Project/Assets/Scripts/BulletController.cs
+++ DDF Project/Assets/Scripts/BulletController.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class BulletController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float m_Speed;
+
+    public void Initialize(float speed)
     {
-        
+        m_Speed = speed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDirection = transform.forward;
+        transform.position += moveDirection * m_Speed * Time.deltaTime;
     }
 }
--- DDF Project/Assets/Scripts/BulletManager.cs
+++ DDF Project/Assets/Scripts/BulletManager.cs
@@ -11,6 +11,7 @@ public class BulletManager : MonoBehaviour
     [Required] public GameObject BulletPrefab;
     public float ShootInterval = 1.0f;
     private float tempShootInterval;
+    public float BulletSpeed = 2.0f;
     [SerializeField] private List<BulletController> bulletList;
     private GameManager gameManager;
 
@@ -36,8 +37,11 @@ public class BulletManager : MonoBehaviour
         Vector3 bulletStartPos = new Vector3(PositionBotton.transform.position.x,
             Random.Range(PositionBotton.transform.position.y, PositionTop.transform.position.y),
             PositionBotton.transform.position.z);
-        GameObject bulletObj = Instantiate(BulletPrefab, bulletStartPos, Quaternion.Euler(0, 0, 0) , BulletGroup.transform);
+        
+        // rotate on y axis -90 faceing left
+        GameObject bulletObj = Instantiate(BulletPrefab, bulletStartPos, Quaternion.Euler(0, -90, 0) , BulletGroup.transform);
         BulletController bullet = bulletObj.GetComponent<BulletController>();
+        bullet.Initialize(BulletSpeed);
         bulletList.Add(bullet);
     }
 }
