afbf40e20b75b5119be8a71da2acf3a7351c2836
Chenshu Xu <newton9001@gmail.com>
2021-02-09 09:31:11 -0800

    传一下

---
+++ DDF Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public PlayerController PlayerController;
+    public ShadowController ShadowController;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        // Dont destroy on reloading the scene
+        DontDestroyOnLoad(gameObject);
+
+        PlayerController = FindObjectOfType<PlayerController>();
+        ShadowController = FindObjectOfType<ShadowController>();
+    }
+}
