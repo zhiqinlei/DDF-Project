65c72a3bcb45285891a1050e9963ff0e0b5873ea
zhiqinlei <22335103+zhiqinlei@users.noreply.github.com>
2021-03-16 17:41:14 -0700

    add final score

---
+++ DDF Project/Assets/FinalScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour
+{
+    // show final socre on end menu
+    public Text scoreText;
+    public int scoreAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = Score.GetScore();
+        scoreText.text = "Final Score: " + scoreAmount + " S";
+        
+    }
+
+}
