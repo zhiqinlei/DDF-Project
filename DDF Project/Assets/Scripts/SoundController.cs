26a38219421bdb84ee85f760828b846a094e024a
yus71 <yuchenshi1234@gmail.com>
2021-04-20 14:39:04 -0700

    add bg bar

--- DDF Project/Assets/Scripts/SoundController.cs
+++ DDF Project/Assets/Scripts/SoundController.cs
@@ -8,21 +8,28 @@ public class SoundController : MonoBehaviour
 
 	public AudioSource asound;
 	public Slider sd;
+    private float musicVolume = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        asound.Play();
+        musicVolume = PlayerPrefs.GetFloat("volume");
+        asound.volume = musicVolume;
+        sd.value = musicVolume;        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(asound.clip.name);       
+        asound.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
         
     }
 
-    public void SoundControl(){
-
-    	asound.volume = sd.value;
+    public void updateVolume(float volume)
+    {
+    	musicVolume = volume;
     }
 
 
